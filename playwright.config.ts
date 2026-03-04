83fd88435f78ab0d0c91475b9b0d81ba1cc08fa0
olenamkolesnik <olenamkolesnik@gmail.com>
2026-03-04 21:30:30 +0200

    feat: update Playwright configuration for trace handling; change trace option to 'on-first-retry' for improved debugging refactor: update assertDeliveryAddress function to use User model; enhance address assertions and improve type safety refactor: remove obsolete response-wrapper tests; clean up unused mock implementations and streamline test suite

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
      baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
     headless: true,
   },
 
--- src/ui/assertions/assert-delivery-address.ts
+++ src/ui/assertions/assert-delivery-address.ts
@@ -1,20 +1,20 @@
 import { expect } from '@playwright/test';
 import { DeliveryAddressUi } from '../models/delivery-address.model';
-import { CreateUserRequest } from '../../api/models/requests/create-user.request';
+import { User } from '../../common/models/product/user.model';
 export function assertDeliveryAddress(
   deliveryAddressUi: DeliveryAddressUi,
-  person: CreateUserRequest,
+  person: User,
 ) {
   expect(deliveryAddressUi.firstNamelastName).toContain(person.title);
-  expect(deliveryAddressUi.firstNamelastName).toContain(person.firstname);
-  expect(deliveryAddressUi.firstNamelastName).toContain(person.lastname);
+  expect(deliveryAddressUi.firstNamelastName).toContain(person.firstName);
+  expect(deliveryAddressUi.firstNamelastName).toContain(person.lastName);
   expect(deliveryAddressUi.address1address2).toContain(person.company);
-  expect(deliveryAddressUi.address1address2).toContain(person.address1);
-  if (person.address2)
-    expect(deliveryAddressUi.address1address2).toContain(person.address2);
-  expect(deliveryAddressUi.cityStatePostcode).toContain(person.city);
-  expect(deliveryAddressUi.cityStatePostcode).toContain(person.state);
-  expect(deliveryAddressUi.cityStatePostcode).toContain(person.zipcode);
-  expect(deliveryAddressUi.country).toBe(person.country);
-  expect(deliveryAddressUi.phone).toBe(person.mobile_number);
+  expect(deliveryAddressUi.address1address2).toContain(person.address?.line1);
+  if (person.address?.line2)
+    expect(deliveryAddressUi.address1address2).toContain(person.address?.line2);
+  expect(deliveryAddressUi.cityStatePostcode).toContain(person.address?.city);
+  expect(deliveryAddressUi.cityStatePostcode).toContain(person.address?.state);
+  expect(deliveryAddressUi.cityStatePostcode).toContain(person.address?.zipCode);
+  expect(deliveryAddressUi.country).toBe(person.address?.country);
+  expect(deliveryAddressUi.phone).toBe(person.mobileNumber);
 }
