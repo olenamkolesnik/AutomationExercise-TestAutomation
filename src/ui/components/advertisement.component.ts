d84ee0a7324904106ebe729df98c5f86f296c434
olenamkolesnik <olenamkolesnik@gmail.com>
2026-03-06 23:14:29 +0200

    refactor: streamline advertisement close button handling in AdvertisementComponent

--- src/ui/components/advertisement.component.ts
+++ src/ui/components/advertisement.component.ts
@@ -1,17 +1,24 @@
 import { Locator, Page } from '@playwright/test';
 
 export class AdvertisementComponent {
-  private readonly adCloseButton: Locator;
+  private readonly closeButtonInPage: Locator;
+  private readonly iframeSelector = 'iframe[name="aswift_3"]';
 
   constructor(private readonly page: Page) {
-    this.adCloseButton = this.page
-      .locator('#dismiss-button')
-      .or(this.page.locator('iframe[name="aswift_3"]').contentFrame().getByRole('button', { name: 'Close ad' }));
+    this.closeButtonInPage = this.page.locator('#dismiss-button');
   }
 
   async closeIfVisible() {
-    if (await this.adCloseButton.isVisible({ timeout: 2000 })) {
-      await this.adCloseButton.click();
+    if (await this.closeButtonInPage.isVisible({ timeout: 2000 }).catch(() => false)) {
+      await this.closeButtonInPage.click();
+      return;
+    }
+
+    const adFrame = this.page.frameLocator(this.iframeSelector);
+    const adCloseButton = adFrame.getByRole('button', { name: 'Close ad' });
+
+    if (await adCloseButton.isVisible({ timeout: 2000 }).catch(() => false)) {
+      await adCloseButton.click();
     }
   }
 }
